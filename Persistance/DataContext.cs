e09d0215f14409fdd5ac3b69d0b28e7c7474c3b7
dev-abhi <career.abhijitrn@gmail.com>
2019-09-10 09:40:57 +0530

    Initial commit for API

---
+++ Persistance/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistance
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+        }
+
+        public DbSet<Value> Values { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Value>()
+                .HasData(
+                    new Value { Id = 1, Name = "Value 101" },
+                    new Value { Id = 2, Name = "Value 201" },
+                    new Value { Id = 3, Name = "Value 301" }
+                );
+        }
+    }
+}
